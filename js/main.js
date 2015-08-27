c2ba7ee15a0e6ae24e621e589db3865ccf4929c7
Timothee Groleau <tgroleau@zendesk.com>
2015-08-27 22:16:15 +0800

    Fancier UI to tweak the parameters

--- js/main.js
+++ js/main.js
@@ -1,51 +1,55 @@
-var max_scale = 2;
-var max_radius = 100;
 
-var m;
-if (m = location.search.match(/\bscale=([0-9.]+)/)) {
-	max_scale = parseFloat(m[1]);
-}
-if (m = location.search.match(/\bradius=(\d+)/)) {
-	max_radius = parseFloat(m[1]);
-}
 
-// Grab the Canvas and Drawing Context
-var canvas = document.getElementById('stage');
-var ctx = canvas.getContext('2d');
+var
+	// tunables
+	max_scale  = 2,
+	max_radius = 100,
+	easing     = 'linear',
 
-var center_x = canvas.width / 2;
-var center_y = canvas.height / 2;
-
-var started = false, img, img2;
+	// Grab the Canvas and Drawing Context
+	canvas = document.getElementById('stage'),
+	ctx = canvas.getContext('2d'),
+	listening = false, img, img2, last_pos = {x: canvas.width / 2, y: canvas.height / 2};
 
 function reset() {
 	img2 = new Image();
 	img2.onload = function() {
 		img = img2;
-		make_stack(center_x, center_y);
-		startListeningToMouse();
+		make_stack(last_pos.x, last_pos.y);
 	};
 	img2.src = 'img/' + $('#staffer').val();
 }
 
-function startListeningToMouse() {
-	if (started) return;
-	started = true;
-	canvas.addEventListener('mousemove', function(evt) {
-		var pos = getMousePos(canvas, evt);
-		make_stack(pos.x, pos.y);
-	}, false);
+function toggleFollowMouse() {
+	if (listening) {
+		canvas.removeEventListener('mousemove', onMouseMove);
+	}
+	else {
+		canvas.addEventListener('mousemove', onMouseMove);
+	}
+
+	listening = !listening;
 }
 
+function onMouseMove(evt) {
+	last_pos = getMousePos(canvas, evt);
+
+	make_stack(last_pos.x, last_pos.y);
+}
+
+// this is where the drawing magic happens
 function make_stack(mx, my) {
+	if (!img) return;
+
 	ctx.clearRect(0,0, canvas.width, canvas.height);
 	ctx.drawImage(img, 0, 0, canvas.width, canvas.height); // scale 1
 
-	var scale = 1;
-	var scale_step = (max_scale - 1) / max_radius;
+	var easing_func = $.easing[easing];
+	var start_scale = 1, end_scale = max_scale;
 
 	for (var radius = max_radius; radius--; ) {
-		scale += scale_step;
+		scale = start_scale + (end_scale - start_scale) * easing_func((max_radius - radius + 1) / max_radius);
+
 		ctx.save();
 		ctx.beginPath();
 		ctx.arc(mx, my, radius, 0, Math.PI * 2);
@@ -70,18 +74,12 @@ function getMousePos(canvas, evt) {
 
 var staff_sel = $('#staffer').change(reset);
 
-// start on a random person
-var num_staff = staff_sel.find('option').length;
-staff_sel.get(0).selectedIndex = Math.floor(Math.random() * num_staff);
+var
+	downloadLink  = $('#downloadLnk'),
+	scale_slider  = $( "#scale_slider"),
+	radius_slider = $( "#radius_slider");
 
-var downloadLink = $('#downloadLnk');
-
-// click on image will take snapshot
-/*
-$('#stage').click(function() {
-	window.open(canvas.toDataURL('png'), 'snapshot');
-});
-/**/
+// click on image will take snapshot and let it download locally
 downloadLink.click(function() {
 	var image_name = $('#staffer').val();
 	var mime = 'image/' + (/\.jpe?g$/i.test(image_name) ? 'jpeg' : 'png');
@@ -90,5 +88,94 @@ downloadLink.click(function() {
 	downloadLink.attr('href', canvas.toDataURL(mime));
 });
 
+function setScale() {
+	max_scale = scale_slider.slider('value');
+	$('#scale_txt').text(max_scale);
+	make_stack(last_pos.x, last_pos.y);
+}
+
+scale_slider.slider({
+	min: 0.2,
+	step: 0.05,
+	max: 3.0,
+	value: max_scale,
+	slide: setScale
+});
+
+function setRadius() {
+	max_radius = radius_slider.slider('value');
+	$('#radius_txt').text(max_radius);
+	make_stack(last_pos.x, last_pos.y);
+}
+
+radius_slider.slider({
+	min: 1,
+	max: 250,
+	value: max_radius,
+	slide: setRadius
+});
+
+$(document).keydown(function(evt) {
+	var key = evt.which;
+	if (key === 32) // space
+		toggleFollowMouse();
+});
+
+// set the easing selector
+var easings = [
+	'linear',
+	'swing',
+	'easeInQuad',
+	'easeInCubic',
+	'easeInQuart',
+	'easeInQuint',
+	'easeInExpo',
+	'easeInSine',
+	'easeInCirc',
+	'easeInElastic',
+	'easeInBounce',
+
+	'easeOutQuad',
+	'easeOutCubic',
+	'easeOutQuart',
+	'easeOutQuint',
+	'easeOutExpo',
+	'easeOutSine',
+	'easeOutCirc',
+	'easeOutElastic',
+	'easeOutBounce',
+
+	'easeInOutQuad',
+	'easeInOutCubic',
+	'easeInOutQuart',
+	'easeInOutQuint',
+	'easeInOutExpo',
+	'easeInOutSine',
+	'easeInOutCirc',
+	'easeInOutElastic',
+	'easeInOutBounce',
+];
+var easing_sel = $('#easing');
+for (var idx = 0; idx<easings.length; idx++) {
+	$(document.createElement('option'))
+		.text(easings[idx])
+		.attr('value', easings[idx])
+		.appendTo(easing_sel);
+}
+easing_sel
+	.val(easing)
+	.change(function() {
+		easing = easing_sel.val();
+		make_stack(last_pos.x, last_pos.y);
+	});
+
+// start on a random person
+var num_staff = staff_sel.find('option').length;
+staff_sel.get(0).selectedIndex = Math.floor(Math.random() * num_staff);
+
 // start!
 reset();
+setScale();
+setRadius();
+toggleFollowMouse();
+
